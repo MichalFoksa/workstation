9f3550faee528e98cec29ec79389a7a7fb4257fc
Michal Foksa <mail@michalfoksa.net>
2019-02-01 10:56:48 +0100

    Prometheus added.

---
+++ src/main/java/net/michalfoksa/workshop/workstation/config/Prometheus.java
@@ -0,0 +1,35 @@
+package net.michalfoksa.workshop.workstation.config;
+
+import javax.inject.Inject;
+
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import net.michalfoksa.workshop.workstation.context.RuntimeContext;
+
+@Configuration
+public class Prometheus {
+
+    @Inject
+    private RuntimeContext runtimeContext;
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> {
+            registry.config().commonTags("application", runtimeContext.getApplicationName()).commonTags("hostname",
+                    runtimeContext.getHostname()).commonTags("ip", runtimeContext.getIp());
+
+            if (runtimeContext.getAllFieldsMap().get("nodeName") != null) {
+                registry.config().commonTags("nodeName", runtimeContext.getAllFieldsMap().get("nodeName").toString());
+            }
+
+            if (runtimeContext.getAllFieldsMap().get("namespace") != null) {
+                registry.config().commonTags("namespace", runtimeContext.getAllFieldsMap().get("namespace").toString());
+            }
+
+        };
+    }
+
+}

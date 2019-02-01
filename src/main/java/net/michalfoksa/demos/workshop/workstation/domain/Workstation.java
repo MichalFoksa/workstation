ca47e4d5f19a3c86d4abfe859399b71ac6b9b887
Michal Foksa <mail@michalfoksa.net>
2019-02-01 17:28:25 +0100

    Do not serialize empty objects.

--- src/main/java/net/michalfoksa/demos/workshop/workstation/domain/Workstation.java
+++ src/main/java/net/michalfoksa/demos/workshop/workstation/domain/Workstation.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
-@JsonInclude(Include.NON_NULL)
+@JsonInclude(Include.NON_EMPTY)
 public class Workstation {
 
 

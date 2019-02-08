f4ecdb6286c1f7e78f8ad37f4a25461f95df3522
Michal Foksa <mail@michalfoksa.net>
2019-02-08 11:56:07 +0100

    Example Swagger contract with Swagger configuration. To generate Api classes execute:
    `java -jar swagger-codegen-cli-2.4.1.jar generate -c codegen-config.json --lang spring -i src/main/resources/swagger/workstation-api.yaml`

---
+++ src/main/java-generated/net/michalfoksa/demos/workshop/workstation/rest/model/Message.java
@@ -0,0 +1,81 @@
+package net.michalfoksa.demos.workshop.workstation.rest.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * A message containing more info why the operation failed
+ */
+@ApiModel(description = "A message containing more info why the operation failed")
+@Validated
+
+public class Message   {
+  @JsonProperty("message")
+  private String message = null;
+
+  public Message message(String message) {
+    this.message = message;
+    return this;
+  }
+
+  /**
+   * The message itself
+   * @return message
+  **/
+  @ApiModelProperty(value = "The message itself")
+
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Message message = (Message) o;
+    return Objects.equals(this.message, message.message);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(message);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Message {\n");
+    
+    sb.append("    message: ").append(toIndentedString(message)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+

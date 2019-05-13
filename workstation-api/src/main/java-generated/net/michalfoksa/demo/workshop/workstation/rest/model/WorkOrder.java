3e4924bd47b28b16b37be5d4a9b2be63b6635189
Michal Foksa <mail@michalfoksa.net>
2019-05-13 16:49:53 +0200

    API classes have got removed somehow from git repo.

---
+++ workstation-api/src/main/java-generated/net/michalfoksa/demo/workshop/workstation/rest/model/WorkOrder.java
@@ -0,0 +1,152 @@
+package net.michalfoksa.demo.workshop.workstation.rest.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import net.michalfoksa.demo.workshop.workstation.rest.model.Workstation;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * Work order for a workstation.
+ */
+@ApiModel(description = "Work order for a workstation.")
+@Validated
+
+public class WorkOrder   {
+  @JsonProperty("workstationName")
+  private String workstationName = null;
+
+  @JsonProperty("parameters")
+  @Valid
+  private Map<String, String> parameters = new HashMap<>();
+
+  @JsonProperty("nextStations")
+  @Valid
+  private List<Workstation> nextStations = new ArrayList<>();
+
+  public WorkOrder workstationName(String workstationName) {
+    this.workstationName = workstationName;
+    return this;
+  }
+
+  /**
+   * Name of the workstation which is supposed to execute the work, e.g.: car body.
+   * @return workstationName
+  **/
+  @ApiModelProperty(required = true, value = "Name of the workstation which is supposed to execute the work, e.g.: car body.")
+  @NotNull
+
+
+  public String getWorkstationName() {
+    return workstationName;
+  }
+
+  public void setWorkstationName(String workstationName) {
+    this.workstationName = workstationName;
+  }
+
+  public WorkOrder parameters(Map<String, String> parameters) {
+    this.parameters = parameters;
+    return this;
+  }
+
+  public WorkOrder putParametersItem(String key, String parametersItem) {
+    this.parameters.put(key, parametersItem);
+    return this;
+  }
+
+  /**
+   * Free named parameters. They do not have any pupose, just to pass some data through assebly line workstations.
+   * @return parameters
+  **/
+  @ApiModelProperty(required = true, value = "Free named parameters. They do not have any pupose, just to pass some data through assebly line workstations.")
+  @NotNull
+
+
+  public Map<String, String> getParameters() {
+    return parameters;
+  }
+
+  public void setParameters(Map<String, String> parameters) {
+    this.parameters = parameters;
+  }
+
+  public WorkOrder nextStations(List<Workstation> nextStations) {
+    this.nextStations = nextStations;
+    return this;
+  }
+
+  public WorkOrder addNextStationsItem(Workstation nextStationsItem) {
+    this.nextStations.add(nextStationsItem);
+    return this;
+  }
+
+  /**
+   * Get nextStations
+   * @return nextStations
+  **/
+  @ApiModelProperty(required = true, value = "")
+  @NotNull
+
+  @Valid
+
+  public List<Workstation> getNextStations() {
+    return nextStations;
+  }
+
+  public void setNextStations(List<Workstation> nextStations) {
+    this.nextStations = nextStations;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    WorkOrder workOrder = (WorkOrder) o;
+    return Objects.equals(this.workstationName, workOrder.workstationName) &&
+        Objects.equals(this.parameters, workOrder.parameters) &&
+        Objects.equals(this.nextStations, workOrder.nextStations);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(workstationName, parameters, nextStations);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class WorkOrder {\n");
+    
+    sb.append("    workstationName: ").append(toIndentedString(workstationName)).append("\n");
+    sb.append("    parameters: ").append(toIndentedString(parameters)).append("\n");
+    sb.append("    nextStations: ").append(toIndentedString(nextStations)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
---
+++ workstation-api/src/main/java-generated/net/michalfoksa/demo/workshop/workstation/rest/model/Workstation.java
@@ -0,0 +1,142 @@
+package net.michalfoksa.demo.workshop.workstation.rest.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * Workstation
+ */
+@ApiModel(description = "Workstation")
+@Validated
+
+public class Workstation   {
+  @JsonProperty("name")
+  private String name = null;
+
+  @JsonProperty("url")
+  private String url = null;
+
+  @JsonProperty("parameters")
+  @Valid
+  private Map<String, String> parameters = new HashMap<>();
+
+  public Workstation name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  /**
+   * Name of the workstation
+   * @return name
+  **/
+  @ApiModelProperty(required = true, value = "Name of the workstation")
+  @NotNull
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Workstation url(String url) {
+    this.url = url;
+    return this;
+  }
+
+  /**
+   * Optional URL of the workstation. Is is appended with `/works`.
+   * @return url
+  **/
+  @ApiModelProperty(value = "Optional URL of the workstation. Is is appended with `/works`.")
+
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public Workstation parameters(Map<String, String> parameters) {
+    this.parameters = parameters;
+    return this;
+  }
+
+  public Workstation putParametersItem(String key, String parametersItem) {
+    this.parameters.put(key, parametersItem);
+    return this;
+  }
+
+  /**
+   * Free named parameters. They do not have any pupose, just to pass some data through assebly line workstations.
+   * @return parameters
+  **/
+  @ApiModelProperty(required = true, value = "Free named parameters. They do not have any pupose, just to pass some data through assebly line workstations.")
+  @NotNull
+
+
+  public Map<String, String> getParameters() {
+    return parameters;
+  }
+
+  public void setParameters(Map<String, String> parameters) {
+    this.parameters = parameters;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Workstation workstation = (Workstation) o;
+    return Objects.equals(this.name, workstation.name) &&
+        Objects.equals(this.url, workstation.url) &&
+        Objects.equals(this.parameters, workstation.parameters);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, url, parameters);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Workstation {\n");
+    
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    url: ").append(toIndentedString(url)).append("\n");
+    sb.append("    parameters: ").append(toIndentedString(parameters)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+

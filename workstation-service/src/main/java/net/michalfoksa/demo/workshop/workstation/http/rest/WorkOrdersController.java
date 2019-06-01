1ce7dc6c4666b394e8e21fa2ecd8e6d60e6044c8
Michal Foksa <mail@michalfoksa.net>
2019-06-01 17:08:57 +0200

    Misc.

--- workstation-service/src/main/java/net/michalfoksa/demo/workshop/workstation/http/rest/WorkOrdersController.java
+++ workstation-service/src/main/java/net/michalfoksa/demo/workshop/workstation/http/rest/WorkOrdersController.java
@@ -10,6 +10,7 @@ import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -74,10 +75,16 @@ public class WorkOrdersController implements WorkOrdersApi {
      *
      */
     @Service
-    public class UriResolver {
+    public class UriResolver implements InitializingBean {
+        Logger log = LoggerFactory.getLogger(UriResolver.class);
 
-        @Value("${service.discovery.client.defaultprototol:http}")
+        @Value("${service.discovery.client.default-prototol:http}")
         private String defaultProtocol;
+
+        @Override
+        public void afterPropertiesSet() throws Exception {
+            log.debug("[defaultProtocol={}]", defaultProtocol);
+        }
 
         /***
          * Create next workstation URI from workstation name, or from
